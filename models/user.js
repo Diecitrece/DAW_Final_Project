14faf8255e561a71c02705f1fdf7f89d3b7fd308
ADCairex <angelmartinezguardiola@gmail.com>
2022-03-24 00:19:24 +0100

    make userSchema

---
+++ models/user.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+var userSchema = mongoose.Schema({
+  _id: {
+    $oid: {
+      type: "ObjectId",
+    },
+  },
+  name: {
+    type: "String",
+  },
+  email: {
+    type: "String",
+  },
+  image: {
+    type: "String",
+  },
+  role: {
+    type: "String",
+  },
+  banned: {
+    type: "Boolean",
+  },
+  emailVerified: {
+    type: "Mixed",
+  },
+});
+
+var users = db.model("users", userSchema);
+
+module.exports = users; // this is what you want

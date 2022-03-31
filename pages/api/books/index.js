48c0246b79c5893d729d6b5dd6979a683c80950a
hectorplinio <hectorplinio@gmail.com>
2022-03-31 16:08:57 +0200

    Method GET, POST, PUT and DELETE done

--- pages/api/books/index.js
+++ pages/api/books/index.js
@@ -9,13 +9,34 @@ export default async function handler (req, res) {
   switch (method) {
     case 'GET':
       try {
-        const books = await Books.find({})
-        res.status(200).json({books})
+        res.status(200).json(await Books.find({}))
       } catch (error) {
         res.status(400).json({ success: false })
       }
       break
     case 'POST':
+      try {
+        const book = new Books(req.body);
+        res.status(200).json(await book.save())
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+      case 'PUT':
+      try {
+        const book = new Books(req.body);
+        await Books.findOneAndUpdate({_id: req.body._id}, book);
+        res.status(200).json(await Books.findOne({_id: req.body._id}))
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+      case 'DELETE':
+      try {
+        res.status(200).json(await Books.deleteOne({_id: req.body._id}))
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
       break
     default:
       break

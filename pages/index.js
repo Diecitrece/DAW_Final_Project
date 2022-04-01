fdbb72ef823b0b72e0d4ab95612ab45658c3e181
ADCairex <angelmartinezguardiola@gmail.com>
2022-04-01 17:41:06 +0200

    edit principal page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,37 @@
+import { useSession, getSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
-export default function Index() {
+const styles = {
+  loginButton:
+    "px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900",
+};
+
+export default function PublicIndex() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  useEffect(() => {
-    console.log(session);
-    if (session) {
-      if (session.user.role == "Admin") {
-        router.push("/admin/indexTest");
-      } else if (session.user.role == "Client") {
-        router.push("/public/indexTest");
-      } else {
-        router.push("/login");
-      }
-    }
-  }, [session]);
+  if (typeof window === "undefined") {
+    return null;
+  }
+  if (session) {
+    return (
+      <>
+        Pagina Principal
+        <button className={styles.loginButton} onClick={() => signOut()}>
+          Logout
+        </button>
+      </>
+    );
+  }
+  router.push("/login");
+  return null;
+}
 
-  return <></>;
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  };
 }

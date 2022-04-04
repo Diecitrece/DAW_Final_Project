26fe8121977ed2f4ccdd91559cc0252e2e9cc8b2
hectorplinio <hectorplinio@gmail.com>
2022-04-04 14:07:56 +0200

    Pass id with query

--- pages/api/books/index.js
+++ pages/api/books/index.js
@@ -9,6 +9,7 @@ export default async function handler (req, res) {
   switch (method) {
     case 'GET':
       try {
+        if (req.query.id) { res.status(200).json(await Books.findById(req.query.id))}
         res.status(200).json(await Books.find({}))
       } catch (error) {
         res.status(400).json({ success: false })

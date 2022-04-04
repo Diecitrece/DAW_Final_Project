b1c95acabb3e26e89f6e89ffeebfec6809363fdb
Juanra08 <juanra08.jrpn@gmail.com>
2022-04-04 14:02:05 +0200

    api-reviews updated

--- pages/api/reviews/index.jsx
+++ pages/api/reviews/index.jsx
@@ -6,23 +6,100 @@ export default async function handler(req, res) {
 
   await dbConnect();
 
-  try {
-    const books = await Books.find({});
+  switch (method) {
+    case "GET":
+      try {
+        const { idReview, idUser } = req.query;
 
-    let reviews = [];
+        if (idReview) {
+          console.log("obtener una");
 
+          const review = await Books.find(
+            { "reviews.idReview": idReview },
+            { reviews: { $elemMatch: { idReview: idReview } } }
+          );
 
-    books.forEach(book => {
-        book.reviews.forEach(review => {
-            console.log(review);
+          res.status(200).json(review[0]);
+        } else if (idUser) {
+          console.log("obtener todas");
 
-            reviews.push(review);
+          const reviews = await Books.find(
+            { "reviews.idUser": idUser },
+            { reviews: { $elemMatch: { idUser: idUser } } }
+          );
 
-        })
-    });
+          res.status(200).json(reviews);
+        } else {
+          console.log("obtener todas");
+          const books = await Books.find({});
 
-    res.status(200).json( reviews );
-  } catch (error) {
-    res.status(400).json({ success: false });
+          const reviews = await Books.find({}, { reviews: 1 });
+
+          res.status(200).json(reviews);
+        }
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const { body } = req;
+        const { idBook, review } = body;
+
+        console.log(idBook, review);
+
+        const reviewExist = await Books.find({
+          "reviews.idReview": review.idReview,
+        });
+
+        if (reviewExist.length === 0) {
+          const book = await Books.findOneAndUpdate(
+            { idBook },
+            { $push: { reviews: review } },
+            { new: true }
+          );
+
+          const books = await Books.find({});
+
+          res.status(200).json(books);
+        } else {
+          const book = await Books.findOneAndUpdate(
+            { idBook },
+            { $pull: { reviews: { idReview: review.idReview } } }
+          );
+
+          const updateBook = await Books.findOneAndUpdate(
+            { idBook },
+            { $push: { reviews: review } },
+            { new: true }
+          );
+
+          const books = await Books.find({});
+
+          res.status(200).json(books);
+        }
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "DELETE":
+      try {
+        const { body } = req;
+        const { idReview } = body;
+
+        const del = await Books.updateOne(
+          { "reviews.idReview": idReview },
+          { $pull: { reviews: { idReview: idReview } } }
+        );
+
+        const books = await Books.find({});
+
+        res.status(200).json(books);
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      break;
   }
 }

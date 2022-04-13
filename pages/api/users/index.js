1cfb57128ae67ea917e88a0d710c90a09da0696d
ADCairex <angelmartinezguardiola@gmail.com>
2022-04-13 23:21:48 +0200

    modify users api to use a filter

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,26 +1,29 @@
-import dbConnect from '../../../lib/dbConnect'
-import Users from '../../../models/user'
+import dbConnect from "../../../lib/dbConnect";
+import Users from "../../../models/user";
 
-export default async function handler (req, res) {
-  const { method } = req
+export default async function handler(req, res) {
+  const { method } = req;
 
-  await dbConnect()
+  await dbConnect();
 
   switch (method) {
-    case 'GET':
+    case "GET":
       try {
-        if (req.query.id) { 
-          res.status(200).json(await Users.findById(req.query.id))
-        }else{
-          res.status(200).json(await Users.find({}))
+        if (req.query.id) {
+          res.status(200).json(await Users.findById(req.query.id));
         }
-            
+        if (req.query.name) {
+          res
+            .status(200)
+            .json(await Users.find({ name: { $regex: req.query.name } }));
+        }
+        res.status(200).json(await Users.find({}));
       } catch (error) {
-        res.status(400).json({ success: false })
+        res.status(400).json({ success: false });
       }
       break;
 
     default:
-      break
+      break;
   }
 }

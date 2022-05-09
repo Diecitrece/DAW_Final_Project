0d437232243ac375baebd07a282fd32cf5bcd021
hectorplinio <hectorplinio@gmail.com>
2022-05-09 18:21:25 +0200

    Update index.js

--- pages/api/requests/index.js
+++ pages/api/requests/index.js
@@ -9,8 +9,8 @@ export default async function handler(req, res) {
 
   switch (method) {
     case "GET":
-      debugger
-      try{
+      debugger;
+      try {
         if (req.query.id) {
           logger.info("REQUEST GET Request by id = " + req.query.id);
           return res.status(200).json(await Request.findById(req.query.id));
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
           logger.info("REQUEST GET Request and find all");
           return res.status(200).json(await Request.find({}));
         }
-      }catch(error){
+      } catch (error) {
         logger.error("ERROR GET Request: " + error);
         return res.status(400).json({ success: false });
       }
@@ -41,7 +41,9 @@ export default async function handler(req, res) {
           const request = new Request(req.body);
           logger.info("REQUEST PUT Request by id = " + req.body._id);
           await Request.findOneAndUpdate({ _id: req.body._id }, request);
-          return res.status(200).json(await Request.findOne({ _id: req.body._id }));
+          return res
+            .status(200)
+            .json(await Request.findOne({ _id: req.body._id }));
         }
         logger.error("REQUEST PUT Request id empty: ");
         return res.status(400).json({ success: false });
@@ -52,7 +54,9 @@ export default async function handler(req, res) {
     case "DELETE":
       try {
         logger.info("REQUEST DELETE Request by id = " + req.body._id);
-        return res.status(200).json(await Request.deleteOne({ _id: req.body._id }));
+        return res
+          .status(200)
+          .json(await Request.deleteOne({ _id: req.body._id }));
       } catch (error) {
         logger.error("ERROR DELETE Request: " + err);
         return res.status(400).json({ success: false });

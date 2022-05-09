1beba44703cebfca6614da14101719cec4ba91c9
hectorplinio <hectorplinio@gmail.com>
2022-05-09 17:32:43 +0200

    book api finish

--- pages/api/books/index.js
+++ pages/api/books/index.js
@@ -1,3 +1,4 @@
+import logger from "../../../components/logger/createLogger";
 import dbConnect from "../../../lib/dbConnect";
 import Books from "../../../models/book";
 
@@ -10,54 +11,68 @@ export default async function handler(req, res) {
     case "GET":
       try {
         if (req.query.id) {
-          res.status(200).json(await Books.findById(req.query.id));
+          logger.info("Request Get Book by id = " + req.query.id);
+          return res.status(200).json(await Books.findById(req.query.id));
         }
         if (req.query.name) {
-          res.status(200).json(
+          logger.info("Request Get Book by name = " + req.query.name);
+          return res.status(200).json(
             await Books.find({
               name: { $regex: req.query.name, $options: "i" },
             })
           );
         }
         if (req.query.ISBN) {
-          res
+          logger.info("Request Get Book by ISBN = " + req.query.ISBN);
+          return res
             .status(200)
             .json(await Books.find({ ISBN: { $regex: req.query.ISBN } }));
         }
-        res.status(200).json(await Books.find({}));
+        logger.info("Request Get Book and find all");
+        return res.status(200).json(await Books.find({}));
       } catch (error) {
-        res.status(400).json({ success: false });
+        logger.error("Error Get Book: " + error);
+        return res.status(400).json({ success: false });
       }
       break;
     case "POST":
       try {
         const book = new Books(req.body);
+        logger.info("Request Post Book: " + book);
         const haveBook = await Books.find({ ISBN: { $regex: book.ISBN } });
         if (haveBook == "") {
-          res.status(200).json(await book.save());
+          logger.info("Request Post, Create new Book: " + book);
+          return res.status(200).json(await book.save());
         }
-        res
+        logger.info("Request Post, Book already exists");
+        return res
           .status(400)
           .json({ success: false, message: "ISBN already exists" });
       } catch (error) {
-        res.status(400).json({ success: false });
+        logger.error("Error Post Book: " + error);
+        return res.status(400).json({ success: false });
       }
       break;
     case "PUT":
       try {
         await Books.findByIdAndUpdate(req.body._id, req.body);
-        res.status(200).json(await Books.findOne({ _id: req.body._id }));
+        logger.info("Request Put Book: " + req.body);
+        return res.status(200).json(await Books.findOne({ _id: req.body._id }));
       } catch (error) {
-        res.status(400).json({ success: false });
+        logger.error("Error Put Book: " + error);
+        return res.status(400).json({ success: false });
       }
       break;
     case "DELETE":
       try {
-        res.status(200).json(await Books.deleteOne({ _id: req.body._id }));
+        logger.info("Request Delete Book: " + req.body);
+        return res
+          .status(200)
+          .json(await Books.deleteOne({ _id: req.body._id }));
       } catch (error) {
-        res.status(400).json({ success: false });
+        logger.error("Error Delete Book: " + error);
+        return res.status(400).json({ success: false });
       }
-      break;
     default:
       break;
   }

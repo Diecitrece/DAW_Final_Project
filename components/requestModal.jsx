33c2297a02960d6619e530a97abbe31bbf668ba9
Alfonso <alfonsopm08@gmail.com@mail.com>
2022-05-11 17:42:45 +0200

    Inde

--- components/requestModal.jsx
+++ components/requestModal.jsx
@@ -154,11 +154,17 @@ export default function MyModal({ isOpen, setIsOpen }) {
                                   className="px-3 py-2 w-full border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                                 />
                               </div>
+                              <label
+                                htmlFor="first_name"
+                                className="block text-sm font-medium leading-5 text-gray-700"
+                              >
+                               Descrpcion
+                              </label>
                               <div className="mt-1 rounded-md shadow-sm">
                                 <textarea
                                   type="text"
                                   id="description"
-                                  name="author"
+                                  name="description"
                                   onChange={handleChange}
                                   placeholder="Description..."
                                   value={form.description}

7d0ba4d006982a843706f5f6267d461b9a641f96
ADCairex <angelmartinezguardiola@gmail.com>
2022-05-12 17:58:57 +0200

    Add delete users and fix reviews without users

--- pages/books/[id].jsx
+++ pages/books/[id].jsx
@@ -58,9 +58,9 @@ export default function LoadBook(props) {
 
   let date = new Date();
 
-  if(session){
+  if (session) {
     var idUser = session.user.id;
-  }else{
+  } else {
     var idUser = "";
   }
 
@@ -172,93 +172,121 @@ export default function LoadBook(props) {
 
                   book.reviews.map((review) => {
                     const user = findUser(review.idUser);
-                    return (
-                      <>
-                        <div className="mb-4" key={review.id}>
-                          <div className="border-2 p-4 rounded-lg">
-                            <div className="flex flex-wrap justify-between">
-                              <div className=" flex flex-wrap mb-4">
-                                <div>
-                                  <img
-                                    src={user.image}
-                                    alt="profile"
-                                    className="rounded-full mr-2 w-12 h-12"
-                                  />
-                                </div>
-                                <div className="">
+                    if (user) {
+                      return (
+                        <>
+                          <div className="mb-4" key={review.id}>
+                            <div className="border-2 p-4 rounded-lg">
+                              <div className="flex flex-wrap justify-between">
+                                <div className=" flex flex-wrap mb-4">
+                                  <div>
+                                    <img
+                                      src={user.image}
+                                      alt="profile"
+                                      className="rounded-full mr-2 w-12 h-12"
+                                    />
+                                  </div>
                                   <div className="">
-                                    <p className="text-xl font-semibold mb-2">
-                                      {user.name}
-                                    </p>
-                                  </div>
-                                  <div className="flex">
-                                    {[...Array(parseInt(review.rating))].map(
-                                      (star, i) => (
-                                        <svg
-                                          key={i}
-                                          className="h-4 w-4 text-orange-500 fill-current mr-1"
-                                          xmlns="http://www.w3.org/2000/svg"
-                                          viewBox="0 0 20 20"
-                                        >
-                                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                                        </svg>
-                                      )
-                                    )}
+                                    <div className="">
+                                      <p className="text-xl font-semibold mb-2">
+                                        {user.name}
+                                      </p>
+                                    </div>
+                                    <div className="flex">
+                                      {[...Array(parseInt(review.rating))].map(
+                                        (star, i) => (
+                                          <svg
+                                            key={i}
+                                            className="h-4 w-4 text-orange-500 fill-current mr-1"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                          >
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                          </svg>
+                                        )
+                                      )}
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                              <div className="text-right">
-                                <div className="">
-                                  <Menu
-                                    as="div"
-                                    className="relative inline-block "
-                                  >
-                                    <div>
-                                      <Menu.Button className="text-xl	font-bold hover:font-extrabold ">
-                                        <i className="fa-solid fa-ellipsis-vertical"></i>
-                                        · · ·
-                                      </Menu.Button>
-                                    </div>
-                                    <Transition
-                                      as={Fragment}
-                                      enter="transition ease-out duration-100"
-                                      enterFrom="transform opacity-0 scale-95"
-                                      enterTo="transform opacity-100 scale-100"
-                                      leave="transition ease-in duration-75"
-                                      leaveFrom="transform opacity-100 scale-100"
-                                      leaveTo="transform opacity-0 scale-95"
+                                <div className="text-right">
+                                  <div className="">
+                                    <Menu
+                                      as="div"
+                                      className="relative inline-block "
                                     >
-                                      <Menu.Items className="focus:outline-none absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-                                        {session.user.id === review.idUser && (
-                                          <>
-                                            <div className="px-1 py-1 ">
-                                              <Menu.Item>
-                                                {({ active }) => (
-                                                  <button
-                                                    className={`${
-                                                      active
-                                                        ? "bg-blue-500 text-white"
-                                                        : "text-gray-900"
-                                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                                    onClick={() => {
-                                                      setOpen(true);
-                                                      setForm({
-                                                        idReview:
-                                                          review.idReview,
-                                                        idUser: session.user.id,
-                                                        pubDate: review.pubDate,
-                                                        description:
-                                                          review.description,
-                                                        rating: review.rating,
-                                                        reports: review.reports,
-                                                      });
-                                                    }}
-                                                  >
-                                                    Editar
-                                                  </button>
-                                                )}
-                                              </Menu.Item>
-                                            </div>
+                                      <div>
+                                        <Menu.Button className="text-xl	font-bold hover:font-extrabold ">
+                                          <i className="fa-solid fa-ellipsis-vertical"></i>
+                                          · · ·
+                                        </Menu.Button>
+                                      </div>
+                                      <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                      >
+                                        <Menu.Items className="focus:outline-none absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                                          {session.user.id ===
+                                            review.idUser && (
+                                            <>
+                                              <div className="px-1 py-1 ">
+                                                <Menu.Item>
+                                                  {({ active }) => (
+                                                    <button
+                                                      className={`${
+                                                        active
+                                                          ? "bg-blue-500 text-white"
+                                                          : "text-gray-900"
+                                                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                      onClick={() => {
+                                                        setOpen(true);
+                                                        setForm({
+                                                          idReview:
+                                                            review.idReview,
+                                                          idUser:
+                                                            session.user.id,
+                                                          pubDate:
+                                                            review.pubDate,
+                                                          description:
+                                                            review.description,
+                                                          rating: review.rating,
+                                                          reports:
+                                                            review.reports,
+                                                        });
+                                                      }}
+                                                    >
+                                                      Editar
+                                                    </button>
+                                                  )}
+                                                </Menu.Item>
+                                              </div>
+                                              <div className="px-1 py-1">
+                                                <Menu.Item>
+                                                  {({ active }) => (
+                                                    <button
+                                                      className={`${
+                                                        active
+                                                          ? "bg-blue-500 text-white"
+                                                          : "text-gray-900"
+                                                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                      onClick={() =>
+                                                        handleDelete(
+                                                          review.idReview
+                                                        )
+                                                      }
+                                                    >
+                                                      Eliminar
+                                                    </button>
+                                                  )}
+                                                </Menu.Item>
+                                              </div>
+                                            </>
+                                          )}
+                                          {session.user.id != review.idUser && (
                                             <div className="px-1 py-1">
                                               <Menu.Item>
                                                 {({ active }) => (
@@ -269,53 +297,32 @@ export default function LoadBook(props) {
                                                         : "text-gray-900"
                                                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                                                     onClick={() =>
-                                                      handleDelete(
-                                                        review.idReview
-                                                      )
+                                                      setRequestOpen(true)
                                                     }
                                                   >
-                                                    Eliminar
+                                                    Reportar review
                                                   </button>
                                                 )}
                                               </Menu.Item>
                                             </div>
-                                          </>
-                                        )}
-                                        {session.user.id != review.idUser && (
-                                          <div className="px-1 py-1">
-                                            <Menu.Item>
-                                              {({ active }) => (
-                                                <button
-                                                  className={`${
-                                                    active
-                                                      ? "bg-blue-500 text-white"
-                                                      : "text-gray-900"
-                                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                                  onClick={() =>
-                                                    setRequestOpen(true)
-                                                  }
-                                                >
-                                                  Reportar review
-                                                </button>
-                                              )}
-                                            </Menu.Item>
-                                          </div>
-                                        )}
-                                      </Menu.Items>
-                                    </Transition>
-                                  </Menu>
+                                          )}
+                                        </Menu.Items>
+                                      </Transition>
+                                    </Menu>
+                                  </div>
                                 </div>
                               </div>
-                            </div>
 
-                            <p className="text-xl mb-4 break-words">
-                              {review.description}
-                            </p>
-                            <p>{review.pubDate}</p>
+                              <p className="text-xl mb-4 break-words">
+                                {review.description}
+                              </p>
+                              <p>{review.pubDate}</p>
+                            </div>
                           </div>
-                        </div>
-                      </>
-                    );
+                        </>
+                      );
+                    }
+                    return;
                   })
                 ) : (
                   /*if there are no reviews, show this message*/

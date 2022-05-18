d93793fe6c5b6591fac3a116ef46e04b5b548d29
Alfonso <alfonsopm08@gmail.com@mail.com>
2022-05-18 10:21:56 +0200

    Estilo panel admin

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,10 +1,11 @@
 import { useSession, getSession, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 import { AdminMenuBar } from "../../components/adminComponents/adminMenuBar";
+import Images from 'next/image'
 
 const styles = {
   loginButton:
-    "px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900",
+    "px-6 py-2 mt-4 text-white bg-blue-800 rounded-lg hover:bg-blue-900 m-5",
 };
 
 export default function AdminIndex() {
@@ -20,14 +21,28 @@ export default function AdminIndex() {
         <>
           <div className="flex flex-row h-full">
             <AdminMenuBar />
-            <div>
-              Admin
-              <br />
-              <button className={styles.loginButton} onClick={() => signOut()}>
-                Logout
-              </button>
-              <br />
-              <a href="/">Public</a>
+            <div className="">
+
+              <div className="text-right m-5">
+              <a href="/"><button className={styles.loginButton}>Parte pública</button></a>
+              <button className={styles.loginButton} onClick={() => signOut()}><i className="fas fa-sign-out-alt text"></i></button>
+              
+              </div>
+
+              <h1 className="font-bold text-4xl text-center">Panel Admin</h1> <br />
+
+              <p className="text-center text-lg">Biblioteca del IES Hermanos Amoros</p>
+              <p className="text-center text-xl">Aquí podras administrar la información de la aplicación</p> <br /> <br />
+
+              <div className="m-10">
+                <h1 className="font-bold text-2xl">Uso de la aplicación en el último año</h1> <br />
+                <Images src="/img/circulo1.jpg" width="600px" height="350px"/>  <Images src="/img/barras1.jpg" width="600px" height="350px"/>
+              </div>
+              
+              <div className="m-10">
+                <h1 className="font-bold text-2xl">Participacion de los usuarios en el último año</h1> <br />
+                <Images src="/img/circulo2.jpg" width="600px" height="350px"/>  <Images src="/img/barras2.jpg" width="600px" height="350px"/>
+              </div>
             </div>
           </div>
         </>
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { PublicNavBar } from "../components/publicNavBar";
 import Link from "next/link";
 
+
 const styles = {
   loginButton:
     "px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900",
@@ -38,17 +39,17 @@ export default function PublicIndex() {
             return (
               <>
 
-                <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-12 mx-auto px-8 py-6 max-w-sm" >
+                <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-lg bg-gray-200 border-gray-400 mt-12 mx-auto px-8 py-6 w-80 text-center" >
                     <a href="">
-                        <img class="rounded-t-lg h-80" src="https://edit.org/photos/img/blog/ppe-crear-portadas-de-libros-online.jpg-840.jpg" alt="" />
+                        <img class="rounded-t-lg h-80 pl-6" src="https://edit.org/photos/img/blog/ppe-crear-portadas-de-libros-online.jpg-840.jpg" alt="" />
                     </a>
                     <div class="p-5">
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{book.name}</h5>
                         </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.name}</p>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.author}</p>
                         <a href={"books/" + book._id} class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Leer más...
+                            Leer más
                             <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </a>
                     </div>

7225aef16ae7ef9fc7a9d06d746741b18ca85c1a
hectorplinio <hectorplinio@gmail.com>
2022-05-18 22:26:00 +0200

    Update new.js

--- pages/admin/books/new.js
+++ pages/admin/books/new.js
@@ -148,7 +148,7 @@ export default function AdminIndex() {
                 </div>
                 <div className="flex items-center justify-between">
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    Editar
+                    Guardar
                   </button>
                 </div>
               </form>

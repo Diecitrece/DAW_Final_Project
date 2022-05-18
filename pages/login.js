67162ff818a88a1d45c69558710cdd21621eacab
hectorplinio <hectorplinio@gmail.com>
2022-05-18 21:49:13 +0200

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ import Images from 'next/image'
 
 const styles = {
   loginButton:
-    "px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 text-center",
+    "flex px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 text-center m-0 ",
 
     
   caja:
